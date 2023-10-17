e0b3f712e5cce172bd2f20260d90b946470d23de
czer <czerpdx@gmail.com>
2023-10-17 14:36:10 -0700

    set up simple hello world using objective-c++

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # bitwise-operators-refresh
-Just a small repo to refresh my memory on how to use bitwise operators.
+Just a small repo to refresh my memory on how to use bitwise operators. Written using `Objective-C++` in `Xcode` to learn basics of working with `C++` and `Objective-C` together.
---
+++ bitwiseRefresh/bitwiseRefresh/main.mm
@@ -0,0 +1,22 @@
+//
+//  main.mm
+//  bitwiseRefresh
+//
+//  Created by Czer on 10/17/23.
+//
+
+// Objective-C imports
+#import <Foundation/Foundation.h>
+
+// C++ includes
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  std::cout << "Hello world from Objective-C++" << std::endl;
+  
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
