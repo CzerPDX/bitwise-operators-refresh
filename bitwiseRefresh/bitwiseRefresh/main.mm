f410f256e9d761f6344c2e5c774349e6c150023d
czer <czerpdx@gmail.com>
2023-10-17 22:01:49 -0700

    tested XOR

--- bitwiseRefresh/bitwiseRefresh/main.mm
+++ bitwiseRefresh/bitwiseRefresh/main.mm
@@ -26,6 +26,26 @@ int main(int argc, const char * argv[]) {
   std::cout << "8 = " << aBitwiseOpsObject.convertDecimalToBinary(8) << std::endl;
   
   
+  // Ok now do some bitwise operations!
+  // Make 0b00000000 look like 0b00101100
+  std::string a = "0b00101100";
+  std::string b = "0b00000000";
+  
+  // First turn a and b into ints
+  int a_int = aBitwiseOpsObject.convertBinaryOctalOrHexToDecimal(a);
+  int b_int = aBitwiseOpsObject.convertBinaryOctalOrHexToDecimal(b);
+  
+  // Perform an XOR
+  int result = a_int ^ b_int;
+  
+  // Then convert back to a binary so can see if they match
+  std::string result_binary = aBitwiseOpsObject.convertDecimalToBinary(result);
+  
+  // Then print out a_int and result_binary to see if they match:
+  std::cout << a << std::endl;
+  std::cout << result_binary << std::endl;
+  
+  
   @autoreleasepool {
       NSLog(@"Hello, World! from Objective-C!!");
   }
